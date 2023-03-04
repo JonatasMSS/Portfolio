844d07e9eb50b5f995acfec728a0e7272a5853ab
JonatasMSS <jonatas.miguelss@gmail.com>
2023-03-04 15:24:48 -0300

    Ambiente preparado

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "./styles/main.css";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Olá mundo</h2>
+    </div>
+  )
+}
+
+export default App
