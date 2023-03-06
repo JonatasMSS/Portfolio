09f67173d1a7a1a04985c24795aae4ab12cf58d4
JonatasMSS <jonatas.miguelss@gmail.com>
2023-03-06 19:58:22 -0300

    feat: added breakpoints library

--- src/App.tsx
+++ src/App.tsx
@@ -104,79 +104,42 @@ function App() {
   ]
 
 
-  // Screens Size
-  const windowWidth = useRef(window.innerWidth);
 
-
-  //Project Controller -> To show diferent displays based on screen size
-
-  function ResponsiveDisplayProjects(width: number) {
-    if (width >= 768) {
-      return (
-        <div className="w-full flex flex-col items-center px-5 mt-20">
-            <span className="font-archivo-black">Projetos</span>
-            <div className=" w-full grid grid-flow-row grid-cols-3 ">
-              {
-                myProjects.map(e => e)
-              }
-            </div>
-        </div>
-      )
-    }
-    return (
-      <div className="flex flex-col w-full items-center gap-5 my-14 px-4">
-        <span className="font-archivo-black">Projetos</span>
-
-        {/* Caroussel */}
-        <div className="flex w-full items-center justify-center gap-2">
-          <button onClick={() => { setProjectIndex(prev => prev === 0 ? (myProjects.length) - 1 : prev - 1) }} >
-            <img src={ButtonCarrousel} alt="Button carousel" className="w-14 -rotate-180" />
-          </button>
-          {
-            myProjects[projectIndex]
-          }
-          <button onClick={() => setProjectIndex(prev => prev >= (myProjects.length - 1) ? 0 : prev + 1)}>
-            <img src={ButtonCarrousel} alt="Button carousel" className="w-14" />
-          </button>
-        </div>
-      </div>
-    )
-  }
 
 
   return (
-    <div className="w-screen min-h-screen flex flex-col justify-between  overflow-hidden bg-defaultWhite">
+    <div className="min-h-screen flex flex-col justify-between overflow-hidden  bg-defaultWhite">
       <div className="flex flex-col">
         <Header />
         {/* Introduction section */}
         <div className="w-full flex flex-col items-center px-4 md:px-10">
           <div
-            className="w-full md:px-4 flex items-center justify-evenly bg-white  m-10 shadow-cartoon border-[3px] rounded-lg border-black">
+            className="w-full px-4 flex items-center justify-evenly bg-white  m-10 shadow-cartoon border-[3px] rounded-lg border-black">
             <div className="font-lato font-bold w-64 md:w-full">
-              <span className="md:text-4xl">Olá, sou Jonatas Miguel. <span className="font-extrabold">Desenvolvedor Front-end</span></span>
+              <span className="md:text-4xl lg:text-5xl">Olá, sou Jonatas Miguel. <span className="font-extrabold">Desenvolvedor Front-end</span></span>
             </div>
-            <img src={Eu} alt="Foto de Jonatas" className="w-20 md:w-32" />
+            <img src={Eu} alt="Foto de Jonatas" className="w-1/4" />
           </div>
         </div>
 
 
         {/* About me and Skills Section */}
 
-        <div className="flex flex-col md:flex-row md:items-center">
+        <div className="flex flex-col md:flex-row md:items-start">
 
           {/* About me section */}
           <div className="flex flex-col items-center px-4 gap-2 w-full">
-            <span className="font-archivo-black">Sobre mim</span>
+            <span className="font-archivo-black md:text-2xl">Sobre mim</span>
             <div className="w-full flex p-2 items-center justify-evenly bg-white min-h-[128px] shadow-cartoon border-[3px] rounded-lg border-black">
-              <span className="font-lato text-sm font-semibold">Sou apaixonado por tecnologia e ciências. Desde pequeno, tive gosto por investigar e experimentar das mais diversas tecnologias. Quando entrei no ensino médio técnico, pude aprofundar meus conhecimentos tanto na robótica, como no curso de desenvolvimento de sistemas. Sempre tentei por em prática os meus conhecimentos: criando robôs, aplicativos, códigos e fazendo muitos experimentos</span>
+              <span className="font-lato text-sm text-start font-semibold md:text-xl lg:text-2xl">Sou apaixonado por tecnologia e ciências. Desde pequeno, tive gosto por investigar e experimentar das mais diversas tecnologias. Quando entrei no ensino médio técnico, pude aprofundar meus conhecimentos tanto na robótica, como no curso de desenvolvimento de sistemas. Sempre tentei por em prática os meus conhecimentos: criando robôs, aplicativos, códigos e fazendo muitos experimentos</span>
             </div>
           </div>
 
           {/* Skills Section */}
 
-          <div className="flex flex-col w-full items-center gap-10 px-4 mt-10">
-            <span className="font-archivo-black">Skills</span>
-            <div className="w-full grid grid-flow-row grid-cols-3 gap-9 px-5">
+          <div className="flex flex-col w-full items-center gap-10 px-4 mt-10 md:mt-0">
+            <span className="font-archivo-black md:text-2xl">Skills</span>
+            <div className="w-full grid grid-flow-row grid-cols-3 gap-9 lg:gap-12 px-5">
               {
                 Skills.map((Skill, i) => (
                   <SkillsContainer
@@ -192,9 +155,22 @@ function App() {
         </div>
         {/* Projects Section */}
 
-        {
-          ResponsiveDisplayProjects(windowWidth.current)
-        }
+        <div className="flex flex-col w-full items-center gap-5 my-14 px-4">
+          <span className="font-archivo-black md:text-2xl">Projetos</span>
+
+          {/* Caroussel */}
+          <div className="flex w-full items-center justify-center gap-2">
+            <button onClick={() => { setProjectIndex(prev => prev === 0 ? (myProjects.length) - 1 : prev - 1) }} >
+              <img src={ButtonCarrousel} alt="Button carousel" className="w-14 -rotate-180" />
+            </button>
+            {
+              myProjects[projectIndex]
+            }
+            <button onClick={() => setProjectIndex(prev => prev >= (myProjects.length - 1) ? 0 : prev + 1)}>
+              <img src={ButtonCarrousel} alt="Button carousel" className="w-14" />
+            </button>
+          </div>
+        </div>
 
 
 
