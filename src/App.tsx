d491039939189fe62ef07952d7dc15ce8e1deceb
JonatasMSS <jonatas.miguelss@gmail.com>
2023-03-07 11:08:49 -0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -111,11 +111,11 @@ function App() {
 
 
   return (
-    <div className="min-h-screen flex flex-col justify-between overflow-hidden  bg-defaultWhite">
-      <div className="flex flex-col">
+    <div className="min-h-screen flex flex-col overflow-hidden  bg-defaultWhite">
+      <div className="flex flex-col ">
         <Header />
         {/* Introduction section */}
-        <div className="w-full flex flex-col items-center px-4 md:px-10">
+        <div className="w-full flex flex-col items-center px-4 md:px-10 lg:px-20">
           <div
             className="w-full px-4 flex items-center justify-evenly bg-white  m-10 shadow-cartoon border-[3px] rounded-lg border-black">
             <div className="font-lato font-bold w-64 md:w-full">
@@ -131,7 +131,7 @@ function App() {
         
 
           {/* About me section */}
-          <div className="flex flex-col items-center px-4 gap-2 w-full">
+          <div className="flex flex-col items-center px-4 lg:px-20 gap-2 w-full">
             <span className="font-archivo-black md:text-2xl">Sobre mim</span>
             <div className="w-full flex p-2 items-center justify-evenly bg-white min-h-[128px] shadow-cartoon border-[3px] rounded-lg border-black">
               <span className="font-lato text-sm text-start font-semibold md:text-xl lg:text-2xl">Sou apaixonado por tecnologia e ciências. Desde pequeno, tive gosto por investigar e experimentar das mais diversas tecnologias. Quando entrei no ensino médio técnico, pude aprofundar meus conhecimentos tanto na robótica, como no curso de desenvolvimento de sistemas. Sempre tentei por em prática os meus conhecimentos: criando robôs, aplicativos, códigos e fazendo muitos experimentos</span>
@@ -140,7 +140,7 @@ function App() {
 
           {/* Skills Section */}
 
-          <div className="flex flex-col items-center gap-10 px-4 mt-10 ">
+          <div className="flex flex-col items-center gap-10 px-4 my-10 ">
             <span className="font-archivo-black md:text-2xl">Skills</span>
             <div className="grid grid-flow-row grid-cols-3 gap-9 md:gap-20 ">
               {
@@ -156,12 +156,13 @@ function App() {
           </div>
 
        
-        {/* Projects Section */}
+        {/* Projects Section */} 
+        <span className="font-archivo-black w-full text-center md:text-2xl">Projetos</span>
+
 
         {
           !isMediumScreen  ? <div className="flex flex-col w-full items-center gap-5 my-14 px-4">
-          <span className="font-archivo-black md:text-2xl">Projetos</span>
-
+         
           {/* Caroussel */}
           <div className="flex w-full items-center justify-center gap-2">
             <button onClick={() => { setProjectIndex(prev => prev === 0 ? (myProjects.length) - 1 : prev - 1) }} >
@@ -175,8 +176,10 @@ function App() {
             </button>
           </div>
         </div> :
-        <div className="w-full grid grid-flow-row grid-cols-3 my-10 bg-red-300 h-32">
-          
+        <div className="p-2 justify-items-center grid grid-flow-row grid-cols-3 my-10 ">
+            {
+              myProjects.map(e => e)
+            }
         </div>
         }
 
